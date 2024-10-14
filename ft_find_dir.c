17b3078ea7fec02e107ed04b185c1ab38e1ba0e4
pabromer <pabromer@student.42malaga.com>
2024-10-14 16:08:26 +0200

    find directions

---
+++ ft_find_dir.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_dir.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabromer <pabromer@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 15:53:36 by pabromer          #+#    #+#             */
+/*   Updated: 2024/10/14 16:06:37 by pabromer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_find_dir(char ** env, const char *str)
+{
+    int     i;
+    char    *result;
+
+    i = 0;
+    while(env[i])
+    {
+        result = ft_strnstr(env[i], str, ft_strlen(str));
+        if(result)
+            return (ft_strchr(result, '/'));
+        i++;
+    }
+    return (NULL);
+}

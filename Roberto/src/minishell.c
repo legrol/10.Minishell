c0a32385ec0ef17878793c8c92915f0b672eca91
Roberto Del Olmo Lima <rdel-olm@c3r4s5.42malaga.com>
2024-10-14 15:01:01 +0200

    Initial

---
+++ Roberto/src/minishell.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 11:33:21 by rdel-olm          #+#    #+#             */
+/*   Updated: 2024/10/03 18:57:37 by rdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+/**
+ * The main function of minishell xxx.
+ * 
+ * @param xxxx
+ * 
+ */
+
+// int	main(int argc, char **argv, char **env)
+// {
+// 	if (argc != 1 || argv[1])
+// 		ft_manage_err(YELLOW NUM_ARGV_ERR RESET);
+// 	if (argc != 1 || env == NULL || *env == NULL)
+// 		ft_manage_err(YELLOW ENV_ERR RESET);
+// 	(void)argc;
+// 	(void)(argv);
+// 	ft_print_init();
+// 	ft_init_minishell(&gl_mshll, env);
+
+
+// }
+
+int main()
+{
+	ft_print_init();
+}

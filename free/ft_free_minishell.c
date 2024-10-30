14e0a38fe61a866f79596dbedc7d659817284680
rdel-olm <rdel-olm@student.42malaga.com>
2024-10-30 10:32:47 +0100

    Update several comments rdel-olm

--- free/ft_free_minishell.c
+++ free/ft_free_minishell.c
@@ -13,17 +13,22 @@
 #include "../includes/minishell.h"
 
 /**
- * The function "ft_free_minishell" xxx
+ * The function "ft_free_minishell" releases all memory allocated within the
+ * `t_minishell` structure, including arrays, linked lists, and strings.
+ * This function is essential for avoiding memory leaks when the shell exits.
  * 
- * @param t_minishell *minishell		xxx
- * 
- * 
+ * @param t_minishell *minishell	A pointer to the `t_minishell` structure
+ *									that holds various dynamically allocated 
+ *									elements such as environment variables, 
+ *									the prompt string, and tokenized command 
+ *									data
+ *  
  */
 
 void	ft_free_minishell(t_minishell *minishell)
 {
-	int i;
-		
+	int	i;
+
 	i = 0;
 	if (minishell->envp)
 	{

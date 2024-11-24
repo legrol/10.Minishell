acbeb74b6860b9a256b7cf22cc80376296781db6
rdel-olm <rdel-olm@student.42malaga.com>
2024-11-24 16:57:15 +0100

    Update

--- libs/libft/ft_strjoin.c
+++ libs/libft/ft_strjoin.c
@@ -11,63 +11,31 @@
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
-#include "../../includes/minishell.h"
-//#define MAX_STR_LEN 4096
-
-
-
-// char	*ft_strjoin(char const *s1, char const *s2)
-// {
-// 	char	*str3;
-// 	int		totallen;
-// 	int		i;
-// 	int		j;
-
-// 	totallen = ft_strlen(s1) + ft_strlen(s2);
-// 	i = 0;
-// 	j = 0;
-// 	str3 = (char *) malloc((totallen + 1) * sizeof(char));
-// 	if (!str3)
-// 		return (0);
-// 	while (s1[i] != '\0')
-// 	{
-// 		str3[i] = s1[i];
-// 		i++;
-// 	}
-// 	while (s2[j] != '\0')
-// 	{
-// 		str3[i] = s2[j];
-// 		i++;
-// 		j++;
-// 	}
-// 	str3[i] = '\0';
-// 	return (str3);
-// }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
+	char	*str3;
 	int		totallen;
 	int		i;
 	int		j;
 
-	if (!s1 || !s2)
-		return (NULL);
 	totallen = ft_strlen(s1) + ft_strlen(s2);
-	// if (totallen <= 0 || totallen > MAX_STR_LEN)
-	// 	return (NULL);
-	result = (char *) malloc((totallen + 1) * sizeof(char));
-	if (!result)
-		return (NULL);
 	i = 0;
-	while (s1[i])
+	j = 0;
+	str3 = (char *) malloc((totallen + 1) * sizeof(char));
+	if (!str3)
+		return (0);
+	while (s1[i] != '\0')
 	{
-		result[i] = s1[i];
+		str3[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (s2[j])
-		result[i++] = s2[j++];
-	result[i] = '\0';
-	return (result);
+	while (s2[j] != '\0')
+	{
+		str3[i] = s2[j];
+		i++;
+		j++;
+	}
+	str3[i] = '\0';
+	return (str3);
 }
